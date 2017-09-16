6cab0e6c81994252c65502d095911bd1f2f85720
isultania <isultania@unomaha.edu>
2017-09-16 16:46:17 -0500

    additional files and changes for Heroku

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,12 @@ SECRET_KEY = 'gy87t3h2z$affj_3qo3&5f66bq9u)o@7hocq7e(wa_v#13#3(f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+try:
+  from .local_settings import *
+except ImportError:
+    pass
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +83,12 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd5oat61pas6ka2',
+        'USER': 'buhiztdqljqnbf',
+        'PASSWORD': '2d9b4a87dc2041502264bc351ec943ea5cffd73358ad89e90a87335c14bd10d2',
+        'HOST': 'ec2-107-20-255-96.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -120,4 +129,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
+STATIC_URL ='/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+os.path.join(PROJECT_ROOT,'static'),
+)
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
